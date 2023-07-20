8b711d22e46eb40a0276b8d49beb80d2ac00c6c0
Anurag kumar <74180720+Anuragk-kumar@users.noreply.github.com>
2023-07-20 13:44:24 +0530

    Add files via upload

---
+++ accounts/static/js/login.js
@@ -0,0 +1,15 @@
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("passwordInput");
+  var togglePassword = document.querySelector(".toggle-password");
+  var icon = togglePassword.querySelector("i");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('' ,  home  , name="home"),
+    path('register/' , register_attempt , name="register_attempt"),
+    path('accounts/login/' , login_attempt , name="login_attempt"),
+    path('token' , token_send , name="token_send"),
+    path('success' , success , name='success'),
+    path('verify/<auth_token>' , verify , name="verify"),
+    path('error' , error_page , name="error"),
+    path('logout/' , Logout , name="logout"),
+    path('change-password/<token>/' , ChangePassword , name="change_password"),
+    path('forget-password/' , ForgetPassword , name="forget_password"),
+
+    
+   
+]
